6aa3ec3c88e9f3a0e359543eac7d1999b6dbd490
Alexander <alvalenzuela2304@gmail.com>
2021-12-21 18:52:18 -0300

    commit java functional programming

---
+++ src/main/java/functionalInterface/_Consumer.java
@@ -0,0 +1,52 @@
+package functionalInterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        // Normal Java function
+        Customer maria = new Customer("María", "99999" );
+        greetCustomer(maria);
+
+        greetCustomerV2(maria, true);
+
+        // Consumer Functional interface
+        greetCostumerConsumer.accept(maria);
+
+        greetCostumerConsumerV2.accept(maria, false);
+    }
+
+    static
+    BiConsumer<Customer, Boolean> greetCostumerConsumerV2 = (customer , showPhoneNumber)->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
+
+    static Consumer<Customer> greetCostumerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName +
+            ", thanks for registering phone number "
+            + customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number "
+                + customer.customerPhoneNumber);
+    }
+
+    static void greetCustomerV2(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number "
+                + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,53 @@
+package streams;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class _Stream {
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("John", Gender.MALE),
+                new Person("María", Gender.FEMALE),
+                new Person("Aisha", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Alice", Gender.FEMALE)
+        );
+
+//        people.stream()
+//                .map(person -> person.name)
+//                .mapToInt(String::length)
+//                .forEach(System.out::println);
+
+        Predicate<Person> personPredicate = person -> Gender.FEMALE.equals(person.gender);
+
+        boolean containsOnlyFemales = people.stream()
+                .allMatch(personPredicate);
+
+        System.out.println(containsOnlyFemales);
+
+
+    }
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
